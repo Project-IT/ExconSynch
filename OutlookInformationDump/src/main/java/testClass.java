e36980608a0bcd0d3c973171d00040eafbbe9ca1
Shayan <shayanf@kth.se>
2017-04-21 10:19:23 +0200

    commented code

--- OutlookInformationDump/src/main/java/testClass.java
+++ OutlookInformationDump/src/main/java/testClass.java
@@ -9,6 +9,9 @@ import microsoft.exchange.webservices.data.credential.WebCredentials;
 import microsoft.exchange.webservices.data.search.CalendarView;
 import microsoft.exchange.webservices.data.search.FindItemsResults;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,41 +25,45 @@ public class testClass {
 
     public static void main(String[] args) throws Exception {
 
-        Scanner sE = new Scanner(System.in);
-        FileWriter fw = new FileWriter("output.json");
+        Scanner sE = new Scanner(System.in);                                                                                // Reads input from standard input device
+        FileWriter fw = new FileWriter("output.JSON");                                                             // Creates a dumpfile
+        JSONObject obj = new JSONObject();                                                                                  // Creates a JSON object
+        JSONArray Body = new JSONArray();                                                                                   // Creates a JSON array for the event body
 
-        System.out.println ("Email address: ");
+        System.out.println("Email address: ");
         String name = sE.next();
         System.out.println("Password: ");
         String pass = sE.next();
 
-        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
-        ExchangeCredentials credentials = new WebCredentials(name, pass);
-        service.setCredentials(credentials);
+        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);                                     // Specifies which Exchange version, though any newer works as well
+        ExchangeCredentials credentials = new WebCredentials(name, pass);                                                    // Log in with the respective Exchange account
+        service.setCredentials(credentials);                                                                                 // Verifies the credentials
 
-        service.autodiscoverUrl(name, new RedirectionUrlCallback());
+        service.autodiscoverUrl(name, new RedirectionUrlCallback());                                                         // Finds the URI for the E-mail
 
 
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date startDate = formatter.parse("2016-05-01 12:00:00");
-        Date endDate = formatter.parse("2017-05-30 13:00:00");
-        CalendarFolder cf = CalendarFolder.bind(service, WellKnownFolderName.Calendar);
-        FindItemsResults<Appointment> findResults = cf.findAppointments(new CalendarView(startDate, endDate));
-        findResults.getItems();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");                                    // Sets the Date Format
+        Date startDate = formatter.parse("2016-05-01 12:00:00");                                                      // Sets start Date
+        Date endDate = formatter.parse("2017-05-30 13:00:00");                                                        // Sets end Date
+        CalendarFolder cf = CalendarFolder.bind(service, WellKnownFolderName.Calendar);                                      // Defines which Calendar Folder to use
+        FindItemsResults<Appointment> findResults = cf.findAppointments(new CalendarView(startDate, endDate));               // Makes an array of Calendar Results
+        findResults.getItems();                                                                                              // Gets items
         for (Appointment appt : findResults.getItems()) {
 
-            appt.load();
+            appt.load();                                                                                                     // Loads event
 
-            String s = appt.getBody().toString();
-            fw.write(s);
+            String s = appt.getBody().toString();                                                                            // Changes the Calendar Event body to a simple String
+            Body.add(s);                                                                                                     // Adds String to JSON Array
         }
+        obj.put("Body List", Body);                                                                                          // Puts Body into JSON Object
+        fw.write(obj.toJSONString());                                                                                        // FileWriter writes the String to the Object
         fw.close();
         System.out.print("done!");
 
     }
 
 
-    static class RedirectionUrlCallback implements IAutodiscoverRedirectionUrl {
+    static class RedirectionUrlCallback implements IAutodiscoverRedirectionUrl {                                             // Simple error checker for te URI
         public boolean autodiscoverRedirectionUrlValidationCallback(
                 String redirectionUrl) {
             return redirectionUrl.toLowerCase().startsWith("https://");
