17e4eec16982da15400ffdd5c775d6d5f122b44a
Aristotelis <kotsias.a@gmail.com>
2017-04-26 15:42:51 +0200

    reformatted and refactored code

--- myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/Event.java
+++ myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/Event.java
@@ -1,34 +1,31 @@
 package com.atlassian.tutorial.macro;
 
-/**
- * Created by Kewser on 4/24/17.
- */
+
 public class Event {
-    String start;  //Start time of the event
-    String end;    //End time of the event
-    String location; //Location of the event
-    String body;  //Body aka the message in the event
-    Boolean allDay; //boolean is the event an all day event?
-    String subject; //event subject
 
-    //What about event name, type etc?
+    String start;       // Start time of the event
+    String end;         // End time of the event
+    String location;    // Location of the event
+    String body;        // Body of the event
+    Boolean allDay;     // Boolean to check whether the event is "all day" event or not
+    String subject;     // Event subject
 
-    public void addStart(String start){
-        this.start = start;
-    }
+    public void addStart(String start) { this.start = start; }
 
     public void addEnd(String end){
         this.end = end;
     }
+
     public void addLocation(String location){
         this.location = location;
     }
+
     public void addBody(String body){
         this.body = body;
     }
-    public void addAllDayEvent(Boolean allDay){
-        this.allDay = allDay;
-    }
+
+    public void addAllDayEvent(Boolean allDay){ this.allDay = allDay; }
+
     public void addSubject(String subject){
         this.subject = subject;
     }
--- myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/ExCon.java
+++ myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/ExCon.java
@@ -4,7 +4,6 @@ import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.macro.Macro;
 import com.atlassian.confluence.macro.MacroExecutionException;
 
-
 import microsoft.exchange.webservices.data.autodiscover.IAutodiscoverRedirectionUrl;
 import microsoft.exchange.webservices.data.core.ExchangeService;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
@@ -24,85 +23,95 @@ import java.util.Map;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
 
-public class helloworld implements Macro {
 
-    public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException {
-        //Scanner sE = new Scanner(System.in);
+public class ExCon implements Macro
+{
+
+    public String execute(Map<String, String> map, String s, ConversionContext conversionContext) throws MacroExecutionException
+    {
+
+        String username =map.get("Username");
+        String password =map.get("Password");
+
+        // Specifies Exchange version, (any newer works as well)
+        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);
+        // Log in with the respective Exchange account
+        ExchangeCredentials credentials = new WebCredentials(username, password);
+        // Verifies the credentials
+        service.setCredentials(credentials);
+
+        // Finds the URI for the E-mail
         try {
-            FileWriter fw = new FileWriter("output.JSON");                                                                      // Creates a dumpfile
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //System.out.println("Email address: ");
-        String name =map.get("Username");
-        // String name = sE.next();
-        //System.out.println("Password: ");
-        //String pass = sE.next();
-        String pass =map.get("Password");
-        StringBuilder output=null;
-        ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2010_SP2);                                     // Specifies which Exchange version, though any newer works as well
-        ExchangeCredentials credentials = new WebCredentials(name, pass);                                                    // Log in with the respective Exchange account
-        service.setCredentials(credentials);                                                                                 // Verifies the credentials
-
-        try {
-            service.autodiscoverUrl(name, new RedirectionUrlCallback());                                                         // Finds the URI for the E-mail
+            service.autodiscoverUrl(username, new RedirectionUrlCallback());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");                                            // Sets the Date Format
-        Date startDate = null;                                                             // Sets start Date
+
+        // Sets the Date Format
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // Sets start Date
+        Date startDate = null;
         try {
             startDate = formatter.parse("2017-04-10 12:00:00");
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        Date endDate = null;                                                               // Sets end Date
+
+        // Sets end Date
+        Date endDate = null;
         try {
             endDate = formatter.parse("2017-05-30 13:00:00");
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        CalendarFolder cf = null;                                      // Defines which Calendar Folder to use
+
+        // Defines which Calendar Folder to use
+        CalendarFolder cf = null;
         try {
             cf = CalendarFolder.bind(service, WellKnownFolderName.Calendar);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        FindItemsResults<Appointment> findResults = null;               // Makes an array of Calendar Results
+
+        // Makes an array of Calendar Results
+        FindItemsResults<Appointment> findResults = null;
         try {
             findResults = cf.findAppointments(new CalendarView(startDate, endDate));
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         findResults.getItems();
         LinkedList<Event> eventsList = new LinkedList<Event>();
-
         for (Appointment appt : findResults.getItems()) {
-            //Loads event
+            // Loads event
             try {
                 appt.load();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            //make a new Event object to hold data of one appointment
+
+            // Make a new Event object to hold data of one appointment
             Event event = new Event();
 
             if (appt != null) {
-                //add subject of the event
+                // Add subject of the event
                 try {
                     event.addSubject(appt.getSubject().toString());
                 } catch (ServiceLocalException e) {
                     e.printStackTrace();
                 }
-                //add is the event all day event
+
+                // Add an "all day" event
                 try {
                     event.addAllDayEvent(appt.getIsAllDayEvent());
                 } catch (ServiceLocalException e) {
                     e.printStackTrace();
                 }
-                //if event is not all day event
+
+                // Add a "not all day" event
                 try {
                     if (!appt.getIsAllDayEvent()) {
                         event.addStart(appt.getStart().toString());
@@ -111,7 +120,8 @@ public class helloworld implements Macro {
                 } catch (ServiceLocalException e) {
                     e.printStackTrace();
                 }
-                //add location if it is not null
+
+                // Add location of the event (if it exists)
                 try {
                     if (appt.getLocation() != null) {
                         event.addLocation(appt.getLocation().toString());
@@ -119,7 +129,8 @@ public class helloworld implements Macro {
                 } catch (ServiceLocalException e) {
                     e.printStackTrace();
                 }
-                //add body
+
+                // Add the body of the event
                 try {
                     event.addBody(appt.getBody().toString());
                 } catch (ServiceLocalException e) {
@@ -127,60 +138,22 @@ public class helloworld implements Macro {
                 }
 
             }
-            //load an event to the linked list eventsList
+            // Load an event to the linked list eventsList
             eventsList.add(event);
 
-
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        for (Appointment appt : findResults.getItems()) {
-
-            try {
-                appt.load();                                                                                                     // Loads event
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            try {
-                Event event = new Event();
-              // s = appt.getSubject().toString();
-
-                event.addSubject(appt.getSubject().toString());
-
-
-            } catch (ServiceLocalException e) {
-                e.printStackTrace();
-            }
-                                                                                                            // Adds String to JSON Array
-        }
-        */
-        //return s;
         return eventsList.toString();
     }
-    static class RedirectionUrlCallback implements IAutodiscoverRedirectionUrl {                                             // Simple error checker for the URI
+
+    // Simple error checker for the URI
+    static class RedirectionUrlCallback implements IAutodiscoverRedirectionUrl {
         public boolean autodiscoverRedirectionUrlValidationCallback(
                 String redirectionUrl) {
             return redirectionUrl.toLowerCase().startsWith("https://");
         }
     }
+
     public BodyType getBodyType() { return BodyType.NONE; }
 
     public OutputType getOutputType() { return OutputType.BLOCK; }
