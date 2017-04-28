51f81b1b5e9231905963323d031e97a72cd11b74
Cecilia Xia <ceciliaX@n130-p237.kthopen.kth.se>
2017-04-28 08:45:43 +0200

    added sql functions

--- myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/ExCon.java
+++ myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/ExCon.java
@@ -22,6 +22,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.sql.*;
 
 
 public class ExCon implements Macro
@@ -142,6 +143,31 @@ public class ExCon implements Macro
 
         }
 
+        try {
+            //1. Get connection to database
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://130.229.188.219:3306/confluence", "tcomkproj2017", "tcomkproj2017");
+            //2. Create a statement
+            Statement myStm = myConn.createStatement();
+            //3. Execute sql query
+
+            //this section is to FETCH data
+            ResultSet myRs = myStm.executeQuery("SELECT * FROM confluence.ao_950dc3_tc_events;");
+            while (myRs.next()) {
+                System.out.println(myRs.getString("SUMMARY"));
+                //see sql queries for more info
+            }
+
+            //This section is to PUSH Data
+            //myStm.executeUpdate("INSERT INTO confluence.ao_950dc3_tc_events (ALL_DAY, CREATED, DESCRIPTION, END, ID, LAST_MODIFIED, LOCATION, ORGANISER, RECURRENCE_ID_TIMESTAMP, RECURRENCE_RULE, REMINDER_SETTING_ID, SEQUENCE, START, SUB_CALENDAR_ID, SUMMARY, URL, UTC_END, UTC_START, VEVENT_UID)\n" +
+            //    "VALUES ('1', '1493235152154', '', '1493251200000', '5', '1493235152154', '', '4028b8815babae10015babb056780000', NULL, NULL, NULL, '0', '1493164800000', 'dfa1eb25-ef12-42c8-abcf-71dec96b58ac', 'Java-pushed event', NULL, '1493244000000', '1493157600000', '20170426T193232Z--2091550207@localhost')");
+
+
+
+            myConn.close(); //closing connection
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
         return eventsList.toString();
     }
 
